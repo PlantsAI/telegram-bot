240bad3c55fd88a14ce8c152a9996a77e7079ee8
Sajjad Aemmi <sajjadaemmi@gmail.com>
2023-05-03 16:34:27 +0330

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# PlantsAI Telegram Bot 🌱
+
+A powerful plant identifier to identify flowers, leaves, trees, and more in seconds!
+
+We would be glad if you use our bot [@PlantsAI_Bot](https://t.me/PlantsAI_Bot).
---
+++ bot.py
@@ -0,0 +1,54 @@
+import os
+import logging
+from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes, InlineQueryHandler
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+
+async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text_caps = ' '.join(context.args).upper()
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+
+async def inline_caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = []
+    results.append(
+        InlineQueryResultArticle(
+            id=query.upper(),
+            title='Caps',
+            input_message_content=InputTextMessageContent(query.upper())
+        )
+    )
+    await context.bot.answer_inline_query(update.inline_query.id, results)
+
+
+if __name__ == '__main__':
+    Token = os.getenv("Token")
+    application = ApplicationBuilder().token(Token).build()
+
+    start_handler = CommandHandler('start', start)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    caps_handler = CommandHandler('caps', caps)
+    inline_caps_handler = InlineQueryHandler(inline_caps)
+
+    application.add_handler(start_handler)
+    application.add_handler(echo_handler)
+    application.add_handler(caps_handler)
+    application.add_handler(inline_caps_handler)
+
+    application.run_polling()
